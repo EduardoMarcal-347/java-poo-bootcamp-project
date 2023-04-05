0af33396fdde0a18af11eb335c2b29a6bb9d8f35
aluno <aluno@udicentro.local>
2023-04-04 22:11:14 -0300

    criando classes Bootcamp e Dev

---
+++ src/br/com/dio/desafio/dominio/Bootcamp.java
@@ -0,0 +1,12 @@
+package br.com.dio.desafio.dominio;
+
+import java.time.LocalDate;
+
+public class Bootcamp {
+
+    private String nome;
+    private String descricao;
+    private LocalDate dataInicio;
+    private LocalDate dataFim;
+
+}
